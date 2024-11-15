e7dde7af99d0e4e13b6f0e90c1a2e9e8e5a237e8
myungkeun02 <audrms3568@gmail.com>
2024-11-15 23:37:46 +0900

    20241115

---
+++ 시간복잡도/_24262.java
@@ -0,0 +1,11 @@
+package 시간복잡도;
+
+public class _24262 {
+    public static void main(String[] args) {
+        // 알고리즘의 수행 횟수
+        System.out.println("1");
+
+        // 알고리즘의 시간 복잡도
+        System.out.println("0");
+    }
+}
